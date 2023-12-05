f3ef8370f90cbf3edf8f75d3277e76e3da31adf6
pzj <ahopeff@gmail.com>
2023-12-05 18:09:09 +0800

    to do app initialized

---
+++ ToDo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from todo_app.models import Task
+
+
+def home(request):
+    tasks = Task.objects.filter(is_completed=False)
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'home.html', context)
